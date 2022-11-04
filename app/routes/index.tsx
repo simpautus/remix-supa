19f392f484ff2aaccf828cb2e2e54292a0a32e64
Simo Toivola <simo.toivola@softdog.fi>
2022-11-04 15:22:01 +0200

    layout

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -42,7 +42,7 @@ export default function IndexRoute() {
 
   return (
     <div className='relative overflow-hidden bg-gray-50 dark:bg-gray-900'>
-      <div className='mx-auto max-w-7xl'>
+      <div className='relative mx-auto max-w-7xl'>
         <div className='relative z-10 bg-gray-50 dark:bg-gray-900 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32'>
           <svg
             className='absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block'
@@ -231,13 +231,13 @@ export default function IndexRoute() {
             </div>
           </main>
         </div>
-      </div>
-      <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
-        <img
-          className='h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full'
-          src='https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80'
-          alt=''
-        />
+        <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
+          <img
+            className='h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full'
+            src='https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80'
+            alt=''
+          />
+        </div>
       </div>
     </div>
   )
